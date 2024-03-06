752ab6c29c73c18422b7713e7e02a250011c5d5e
Samiran_Das <samiran4209@gmail.com>
2024-03-06 18:34:29 +0530

    understanding postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -24,7 +24,7 @@ const registerUser = asyncHandeler(async (req, res) => {
     throw new ApiError(400, "all fields are required");
   }
 
-  const existedUser = User.findOne({
+  const existedUser =await User.findOne({
     $or: [{ username }, { email }],
   });
 
