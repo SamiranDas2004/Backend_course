1cc046d912d3b2bda269e4791d7b383cd5cdcb0d
Samiran_Das <samiran4209@gmail.com>
2024-03-12 13:26:43 +0530

    changed password meathod

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -226,5 +226,24 @@ const refreshAccessToken= asyncHandeler(async(req,res)=>{
   }
 })
 
+const changeCurrentPassword=asyncHandeler(async(req,res)=>{
+  
+  const {oldPassword,newPassword}=req.body
+  
+  const user= await User.findById(req.user?.id)
+
+ const isPasswordCorrect=await user.isPasswordCorrect(oldPassword)
+if (!isPasswordCorrect) {
+  throw new ApiError(400,'invalid password')
+}
+
+user.password=newPassword;
+await user.save({validateBeforeSave:false})
+
+return res
+.status(200)
+.json(new ApiResponse(200,"password is changed"))
+})
+
 
 export { registerUser, loginUser,logoutUser,refreshAccessToken };
