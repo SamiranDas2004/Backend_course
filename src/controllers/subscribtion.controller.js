3268b1609529cfa7e68dfa3a74cbb8d59dab7e4e
Samiran_Das <samiran4209@gmail.com>
2024-03-18 17:30:19 +0530

    toggleSubscription meathod is created

--- src/controllers/subscribtion.controller.js
+++ src/controllers/subscribtion.controller.js
@@ -9,11 +9,53 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400,"the channelId is not valid")
+    }
+
+    const isSubscribed= await Subscription.findOne({
+        subscriber: req.user?._id,
+        channel: channelId,
+    })
+    if (isSubscribed) {
+     await Subscription.findByIdAndDelete(isSubscribed?._id)
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { subscribed: false },
+                "unsunscribed successfully"
+            )
+        )
+    }
+
+    else{ 
+          await Subscription.create({
+        subscriber: req.user?._id,
+        channel: channelId,
+    });
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { subscribed: true },
+                "subscribed successfully"
+            )
+        );
+    }
+   
+
+    
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+    // 
     const {channelId} = req.params
+
 })
 
 // controller to return channel list to which user has subscribed
