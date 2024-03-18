9a2629660e263a7367a8ab6f7a4c536ec6897eb1
Samiran_Das <samiran4209@gmail.com>
2024-03-19 02:08:26 +0530

    create playlist and getUserPlaylist meathod

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -6,14 +6,74 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 
 const createPlaylist = asyncHandler(async (req, res) => {
+    
+    //TODO: create playlist
     const {name, description} = req.body
 
-    //TODO: create playlist
+    if(!name && !description){
+        throw new ApiError(400,"name and description is required");
+    }
+
+    const playList= await Playlist.create({
+        name:name,
+        description,
+        owner:req.user?._id
+    })
+
+    if(!playList){
+        throw new ApiError(400,"playlist is not created")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,playList,"playlist created successfully"))
+
+    
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
-    const {userId} = req.params
+   
     //TODO: get user playlists
+    const {userId} = req.params
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400,"userId is not valid")
+    }
+    const playlists = await Playlist.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "videos",
+                foreignField: "_id",
+                as: "videos"
+            }
+        },
+        {
+            $addFields: {
+                totalVideos: {
+                    $size: "$videos"
+                },
+                totalViews: {
+                    $sum: "$videos.views"
+                }
+            }
+        },
+        {
+            $project: {
+                _id: 1,
+                name: 1,
+                description: 1,
+                totalVideos: 1,
+                totalViews: 1,
+                updatedAt: 1
+            }
+        }
+    ])
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
